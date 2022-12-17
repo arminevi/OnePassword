304913de4364e54478b1b9f896dedd5cd379d7d5
arminevi <arminsaderi@gmail.com>
2022-12-17 16:54:33 +0100

    Added Accordeon with new dump data

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,10 +12,44 @@ class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        $dumpMails = ['dump@gmail.com', 'dump@web.de', 'dump@yahoo.com'];
-        #dd($dumpMails);
+        $dumpUser = "Armin";
+        $dumpPassword = array();
+        $dumpPassword['user'] = array(
+            "1" =>array(
+                "id" => 1,
+                "title" => "Google Konto",
+                "user" => "TopG",
+                "email" => "dump@gmail.com",
+                "password" => "12345"
+            ),
+            "2" =>array(
+                "id" => 2,
+                "title" => "Nike Club",
+                "user" => "",
+                "email" => "dump@gmail.com",
+                "password" => "MichaelJordan1"
+            ),
+            "3" =>array(
+                "id" => 3,
+                "title" => "Amazon",
+                "user" => "",
+                "email" => "dump@gmail.com",
+                "password" => "JeffBezos24"
+            ),
+            "4" =>array(
+                "id" => 4,
+                "title" => "Playstation",
+                "user" => "xXGamerHDXx",
+                "email" => "dump@gmail.com",
+                "password" => "playsiOne"
+            )
+        );
+        
+        #dd($dumpPassword);
+
         return $this->render('index.html.twig', [
-            'emails' => $dumpMails
+            'data' => $dumpPassword,
+            'user' => $dumpUser
         ]);
     }
 }
