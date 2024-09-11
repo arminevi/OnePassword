65ae98831bb99d751078a8caa5d74186091e69a0
Armin Saderi <saderi@hob-by-horse.de>
2024-09-11 10:08:01 +0200

    Added configuration to crud controller / easyadmin

--- src/Controller/PasswordCrudController.php
+++ src/Controller/PasswordCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Password;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class PasswordCrudController extends AbstractCrudController
 {
@@ -20,14 +22,27 @@ class PasswordCrudController extends AbstractCrudController
         return $password;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Passwort')
+            ->setEntityLabelInPlural('Passwörter')
+            ->setPageTitle('new', '%entity_label_singular% erstellen')
+            ->setPageTitle('edit', '%entity_label_singular% bearbeiten')
+            ->setSearchFields(['title'])
+            ->setDefaultSort(['title' => 'ASC'])
+            ->setPaginatorPageSize(15)
+            ->setPaginatorRangeSize(2)
+            ;
+    }
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            'title',
+            'username',
+            'email',
+            'password',
+            TextareaField::new('description')->hideOnIndex(),
         ];
     }
-    */
 }
